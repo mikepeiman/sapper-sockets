02c552cfb2a9ff457967d46001c08cd1a2a5cc1e
Mike Peiman <mikepeiman@gmail.com>
2020-03-13 11:21:04 -0400

    Fixed a menu css issue from raster, renamed about to chat

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -10,17 +10,18 @@
     // setTimeout(() => {
     //   sapper.classList.remove("transition");
     // }, 1000);
-
     // document.documentElement.classList.toggle("transition");
   });
 </script>
 
 <style lang="scss" global>
-  :global(#sapper) {
+  #sapper {
     background: var(--custom-page-bg1);
     transition: all 01.25s;
     z-index: 1;
     position: relative;
+    width: 100%;
+    height: 100%;
     &:before {
       content: "";
       background: var(--custom-page-bg2);
@@ -29,8 +30,7 @@
       left: 0;
       right: 0;
       bottom: 0;
-      // width: 100%;
-      // height: 100%;
+
       transition: all 1s;
       opacity: 0;
       z-index: -1;
@@ -39,19 +39,7 @@
       opacity: 1;
     }
   }
-  :global(.main) {
-    background: var(--custom-page-bg);
-    transition: all 01.25s;
-  }
-  :global(body) {
-    background: var(--custom-page-bg);
-    transition: all 01.25s;
-  }
-  .transition {
-    background: var(--custom-page-bg);
 
-    /* transition: all 0.25s; */
-  }
   main {
     position: relative;
     max-width: 100vw;
@@ -59,6 +47,15 @@
     padding: 2em;
     margin: 0 auto;
     box-sizing: border-box;
+  }
+
+  nav ul li {
+          margin-top: 0;
+      margin-bottom: 0;
+    // &:first-child, &:last-child {
+    //   margin-top: 0;
+    //   margin-bottom: 0;
+    // }
   }
 </style>
 
